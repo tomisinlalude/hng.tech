e3d616d306eef7b545e80eb993a942f1b3613632
maduabuchi <maduabuchiokonkwo@gmail.com>
2019-04-03 20:29:57 +0100

    maduabuchi

--- index.php
+++ index.php
@@ -25,7 +25,6 @@
       '/interns/current' => 'current.php',
       '/become-intern' => 'become-intern.php',
       '/template' => 'interns/template.php',
-      '/maduabuchi' => 'interns/maduabuchi.php',
       '/test' => 'interns/test.php'
   ]);
   $request = $_SERVER['REQUEST_URI'];
