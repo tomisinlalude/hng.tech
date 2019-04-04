b775c1000cdac04a487e4c1d1e68299c260aaa21
davidKathoh <mudavidkat@gmail.com>
2019-04-04 17:35:49 +0200

    DavidKathoh.php edited

--- views/interns/DavidKathoh.php
+++ views/interns/DavidKathoh.php
@@ -11,23 +11,23 @@
                <div class="col-md-12 col-sm-6 col-xs-12">
                   <div class="col-md-6 col-xs-12">
                      <div class="intern-block border-blue">
-                        <img src="https://res.cloudinary.com/iambeejayayo/image/upload/v1547954566/fav-500.png" alt="Mark Essien">
-                        <h6>Mark Essien</h6>
-                        <p>Front-end Engineer</p>
+                        <img src="https://res.cloudinary.com/davidkathoh/image/upload/a_hflip/v1554390709/profile.jpg" alt="david kathoh ">
+                        <h6>David Kathoh</h6>
+                        <p>Android Developer</p>
                         <ul class="list">
-                           <li><a href="#" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
-                           <li><a href="#" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
-                           <li><a href="#" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                           <li><a href="https://twitter.com/DavidKathoh" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/davidkathoh" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@davidkathoh" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
                         </ul>
                      </div>
                   </div>
                   <div class="col-md-6 col-xs-12 align-items-center">
                      <div class="intern-bio border-blue">
                         <h6>About me</h6>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dicta, ipsam aliquam fugit voluptatum ad. Nihil sit ipsum consectetur nulla? Nesciunt illum mollitia repellat corrupti deleniti recusandae, repellendus eaque exercitationem?lore</p>
+                        <p>A software developer with proven ability to build user-centered and cutting-edge android applications. I am a natural problem solver who uses a combination of analytics, customer insights, and intuition to develop new products and features. </p>
                         <br />
                         <h6>Skills</h6>
-                        <p>JavaScript | PHP | Python</p>
+                        <p>Android SDK | JAVA | Python</p>
                      </div>
                   </div>
                </div>
