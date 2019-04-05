77092cafe99d35fc7a1fa85c01f784452bde4396
maduabuchi <maduabuchiokonkwo@gmail.com>
2019-04-05 09:05:14 +0100

    maduabuchi's profile

--- views/interns/maduabuchi.php
+++ views/interns/maduabuchi.php
@@ -13,7 +13,7 @@
                      <div class="intern-block border-blue">
                         <img src="https://res.cloudinary.com/maduflavins/image/upload/v1554309932/4hngintern.png">
                         <h6>Maduabuchi Okonkwo</h6>
-                        <p>Data Analyst</p>
+                        <p>Software Developer</p>
                         <ul class="list">
                            <li><a href="https://twitter.com/MaduabuchiOkon1" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
                            <li><a href="https://github.com/Maduflavins" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
@@ -24,7 +24,7 @@
                   <div class="col-md-6 col-xs-12 align-items-center">
                      <div class="intern-bio border-blue">
                         <h6>About me</h6>
-                        <p>I am Data-Analyst, A software developer and a Machine leraning ethusiast. I believe that in future of Machine Learning</p>
+                        <p>I am Data-Analyst, A software developer a Data-Analyst an IT support Specialist</p>
                         <br />
                         <h6>Skills</h6>
                         <p>Machine Learning | Tableau | Python</p>
