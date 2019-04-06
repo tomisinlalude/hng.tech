e3014ab37c1ad1b11954b9aff42d4f2e32b91286
oviepaulej <oviepe@gmail.com>
2019-04-06 07:45:56 +0100

    add my profile

---
+++ views/interns/oviepaulej.php
@@ -0,0 +1,39 @@
+
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/oviepaulej/image/upload/v1554512490/Ovie_Square.png" alt="Ovie Paul EJ">
+                        <h6>Ovie Paul-Ejukorlem</h6>
+                        <p>Designer</p>
+                        <ul class="list">
+                           <li><a href="https://www.twitter.com/oviepaulej" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://www.github.com/oviepaulej" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://www.medium.com/oviepaulej" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Just an anime loving guy with a passion for design, animation, and all things strategy!</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>Design | Animation | Strategy</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
