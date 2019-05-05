8df07f4355b09ce804cb1043ce34d5cf5c87a793
Charles Dim <dimcharles69@gmail.com>
2019-05-05 03:35:01 +0100

    added final features to product page

--- views/products.php
+++ views/products.php
@@ -4,56 +4,98 @@
 	<?php include 'partials/navbar.php';?>
 	<h1 class="text-center productsHeader" style="margin-bottom: 5rem; font-size: 3.25em;">Products Created by Interns</h1>
 
-	<div class="container gridContainer" style="margin-bottom: 15rem;">
-		<div class="row">
-			<a href="#" id="apoe">
-				<div class="jumbotron product col-lg-4 col-md-6" style="position: relative; z-index: 2;">
-					<!-- <span class="container-fluid detail" style="position: absolute;">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="row center-block" style="width:50%;">
-					</span> -->
+	<div class="container gridContainer">
+		<div class="">
+			<a href="#" class="productLink" id="productAnime">
+				<div class="jumbotron product col-lg-4 col-md-6 ">
+					
 					
 					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="row center-block" style="width:50%;">
-						
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1557018690/apoewhite.png" alt="apoe4_logo" class=" centre-block" style="width:50%; margin:50px 0px 0px 82px;">
+						<p class="text-center detail" >An exciting new app that helps people with history of Apoe4 gene that are culpable to Alhzemier. This product helps users monitor their day to day activities and also provides tips from our medical researchers.</p>
+						<div class="text-center">
+							<button type="button" class="btn btn-custom mr-0">Learn more</button>
+						</div>
+					</div>
+
 				</div>
 			</a>
-			<a href="#">
+			<a href="#" class="productLink" id="productAnime">
 
-				<div class="jumbotron product col-lg-4 col-md-6 text-center">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class="row center-block" style="width:70%;">
+				<div class="jumbotron product  col-lg-4 col-md-6 text-center">
+					
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class=" center-block" style="width:70%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class=" centre-block" style="width:70%; margin: 50px 0px 0px 0px;">
+						<p class="text-center detail">
+						The Ultimate LightWeight Content Platform. Ziki was built on the premise that ownership and privacy is about respect and that it is your right.</p>
+						<div class="text-center">
+							<button type="button" class="btn btn-custom mr-0">Learn more</button>
+						</div>
+
+					</div>
 
 				</div>
 			</a>
-			<a href="#">
+			<a href="#" class="productLink" id="productAnime">
 				<div class="jumbotron product col-lg-4 col-md-6">
 					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="row center-block" style="width:30%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="" class=" centre-block" style="width:30%; margin:50px 0px 0px 115px;">
+						<p class="text-center detail" >
+						Dev/Alert is an online remote job operating system that connects African developers to the rest of the world, it allows developers from anywhere in Africa.</p>
+						<div class="text-center">
+							<button type="button" class="btn btn-custom mr-0">Learn more</button>
+						</div>
+
+					</div>
 				</div>
 			</div>
 		</a>
-		<div class="row">
-			<a href="#">
+
+		<div class="" id="">
+			<a href="#" class="" id="productAnime">
 				<div class="jumbotron product col-lg-4 col-md-6" >
 					
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556806436/Kidslogo.png" alt="apoe4_logo" class="row center-block" style="width:50%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556806436/Kidslogo.png" alt="apoe4_logo" class="center-block" style="width:50%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556806436/Kidslogo.png" alt="" class=" centre-block" style="width:50%; margin: 50px 0px 0px 82px;">
+						<p class="text-center detail" >This beautiful short kid stories would help your children sleep peacefully.A great resource for parents, babysitters, aunts, uncles, grandparents, or anyone in love with classic bedtime stories.</p>
+						<div class="text-center">
+							<button type="button" class="btn btn-custom mr-0">Learn more</button>
+						</div>
+					</div>
 
 				</div>
 			</a>
-			<a href="#">
+			<a href="#" class="productLink" id="ziki2">
 
 				<div class="jumbotron product col-lg-4 col-md-6 text-center">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class="row center-block" style="width:70%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class=" center-block" style="width:70%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class=" centre-block" style="width:70%">
+					</div>
 
 				</div>
 			</a>
-			<a href="#">
+			<a href="#" class="productLink">
 				<div class="jumbotron product col-lg-4 col-md-6">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="row center-block" style="width:30%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class=" center-block" style="width:30%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class=" centre-block" style="width:70%">
+					</div>
 				</div>
 			</div>
 		</a>
-		<div class="row">
+
+		<div class="">
 			<a href="#">
 				<div class="jumbotron product col-lg-4 col-md-6" >
 					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="row center-block" style="width:50%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class="row centre-block" style="width:70%">
+					</div>
 
 				</div>
 			</a>
@@ -61,12 +103,18 @@
 
 				<div class="jumbotron product col-lg-4 col-md-6 text-center">
 					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class="row center-block" style="width:70%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class="row centre-block" style="width:70%">
+					</div>
 
 				</div>
 			</a>
 			<a href="#">
 				<div class="jumbotron product col-lg-4 col-md-6">
 					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="row center-block" style="width:30%;">
+					<div id="extra">
+						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class="row centre-block" style="width:70%">
+					</div>
 				</div>
 			</div>
 		</a>
