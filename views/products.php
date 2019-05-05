90d229e2a52c3e62fb1a642bac7f3efd054fe2be
Charles Dim <dimcharles69@gmail.com>
2019-05-05 09:30:13 +0100

    modified products page

--- views/products.php
+++ views/products.php
@@ -10,7 +10,7 @@
 				<div class="jumbotron product col-lg-4 col-md-6 ">
 					
 					
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="row center-block" style="width:50%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="imgCenter center-block" style="width:50%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1557018690/apoewhite.png" alt="apoe4_logo" class=" centre-block" style="width:50%; margin:50px 0px 0px 82px;">
 						<p class="text-center detail" >An exciting new app that helps people with history of Apoe4 gene that are culpable to Alhzemier. This product helps users monitor their day to day activities and also provides tips from our medical researchers.</p>
@@ -25,7 +25,7 @@
 
 				<div class="jumbotron product  col-lg-4 col-md-6 text-center">
 					
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class=" center-block" style="width:70%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class="imgCenter center-block" style="width:70%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class=" centre-block" style="width:70%; margin: 50px 0px 0px 0px;">
 						<p class="text-center detail">
@@ -40,7 +40,7 @@
 			</a>
 			<a href="#" class="productLink" id="productAnime">
 				<div class="jumbotron product col-lg-4 col-md-6">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="row center-block" style="width:30%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="imgCenter center-block" style="width:30%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="" class=" centre-block" style="width:30%; margin:50px 0px 0px 115px;">
 						<p class="text-center detail" >
@@ -58,7 +58,7 @@
 			<a href="#" class="" id="productAnime">
 				<div class="jumbotron product col-lg-4 col-md-6" >
 					
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556806436/Kidslogo.png" alt="apoe4_logo" class="center-block" style="width:50%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556806436/Kidslogo.png" alt="apoe4_logo" class="imgCenter center-block" style="width:50%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556806436/Kidslogo.png" alt="" class=" centre-block" style="width:50%; margin: 50px 0px 0px 82px;">
 						<p class="text-center detail" >This beautiful short kid stories would help your children sleep peacefully.A great resource for parents, babysitters, aunts, uncles, grandparents, or anyone in love with classic bedtime stories.</p>
@@ -72,7 +72,7 @@
 			<a href="#" class="productLink" id="ziki2">
 
 				<div class="jumbotron product col-lg-4 col-md-6 text-center">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class=" center-block" style="width:70%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class="imgCenter center-block" style="width:70%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class=" centre-block" style="width:70%">
 					</div>
@@ -81,7 +81,7 @@
 			</a>
 			<a href="#" class="productLink">
 				<div class="jumbotron product col-lg-4 col-md-6">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class=" center-block" style="width:30%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="imgCenter center-block" style="width:30%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class=" centre-block" style="width:70%">
 					</div>
@@ -92,7 +92,7 @@
 		<div class="">
 			<a href="#">
 				<div class="jumbotron product col-lg-4 col-md-6" >
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="row center-block" style="width:50%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="imgCenter center-block" style="width:50%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class="row centre-block" style="width:70%">
 					</div>
@@ -102,7 +102,7 @@
 			<a href="#">
 
 				<div class="jumbotron product col-lg-4 col-md-6 text-center">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class="row center-block" style="width:70%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="ziki_logo" class="imgCenter center-block" style="width:70%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class="row centre-block" style="width:70%">
 					</div>
@@ -111,7 +111,7 @@
 			</a>
 			<a href="#">
 				<div class="jumbotron product col-lg-4 col-md-6">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="row center-block" style="width:30%;">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert_logo" class="imgCenter center-block" style="width:30%;">
 					<div id="extra">
 						<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556546640/zikilogo.png" alt="" class="row centre-block" style="width:70%">
 					</div>
