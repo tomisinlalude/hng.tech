37c23bafa85af8aacecaec370b80baf989868c54
oviepaulej <oviepe@gmail.com>
2019-05-14 20:15:07 +0100

    fix card back flip and remove white h1s

--- views/hng5-finalists.php
+++ views/hng5-finalists.php
@@ -28,13 +28,13 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 <div class=" jumbotron-fluid">
 	<div class="JumbHeaderImg">
 		<?php include 'partials/navbar.php'; ?>
-		<main>
+		<main class="container">
 			<header class="hng-finalists-header">
 				<h2>Our HNG 5.0 Finalists</h2>
 				<h3>Here are our finalists from the just concluded internship</h3>
 			</header>
 			<section class="hng-track">
-				<h1>Web Developers</h1>
+				<h1 style="visibility:hidden">Web Developers</h1>
 				<h3>Web Developers</h3>
 			</section>
 
@@ -51,24 +51,25 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 									<p class="intern-desc"><?= $web['Role'] ?></p>
 									<p><?= $web['Stacks'] ?></p>
 								</div>
-								<div class="intern-card-back">
-									<p><?= $web['Name'] ?></p>
-									<p><?= $web['Role'] ?></p>
-									<button><a href="<?= $web['Portfolio'] ?>">View Portfolio</a></button>
-									<div class="social-links">
-										<a href="<?= $web['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
-										<a href="<?= $web['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
-										<a href="<?= $web['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
-									</div>
+							</div>
+							<div class="intern-card-back">
+								<p><?= $web['Name'] ?></p>
+								<p><?= $web['Role'] ?></p>
+								<button><a href="<?= $web['Portfolio'] ?>">View Portfolio</a></button>
+								<div class="social-links">
+									<a href="<?= $web['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
+									<a href="<?= $web['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
+									<a href="<?= $web['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
 								</div>
 							</div>
 						</div>
 					</div>
+					
 				<?php } ?>
 			</div>
 
 			<section class="hng-track">
-				<h1 class="hng-first-track">Designers</h1>
+				<h1 class="hng-first-track" style="visibility:hidden">Designers</h1>
 				<h3>Designers</h3>
 			</section>
 
@@ -85,17 +86,17 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 									<p class="intern-desc"><?= $designer['Role'] ?></p>
 									<p><?= $designer['Stacks'] ?></p>
 								</div>
-								<div class="intern-card-back">
-									<p><?= $designer['Name'] ?></p>
-									<p><?= $designer['Role'] ?></p>
-									<button><a href="<?= $designer['Portfolio'] ?>">View Portfolio</a></button>
-									<div class="social-links">
-										<a href="<?= $designer['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
-										<a href="<?= $designer['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
-										<a href="<?= $designer['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
-										<a href="<?= $designer['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
-										<a href="<?= $designer['Dribbble'] ?>"><img src="../app/img/dribbble icon.png" alt="social link" title="Dribbble"></a>
-									</div>
+							</div>	
+							<div class="intern-card-back">
+								<p><?= $designer['Name'] ?></p>
+								<p><?= $designer['Role'] ?></p>
+								<button><a href="<?= $designer['Portfolio'] ?>">View Portfolio</a></button>
+								<div class="social-links">
+									<a href="<?= $designer['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
+									<a href="<?= $designer['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
+									<a href="<?= $designer['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
+									<a href="<?= $designer['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
+									<a href="<?= $designer['Dribbble'] ?>"><img src="../app/img/dribbble icon.png" alt="social link" title="Dribbble"></a>
 								</div>
 							</div>
 						</div>
@@ -104,7 +105,7 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 			</div>
 
 			<section class="hng-track">
-				<h1>Mobile Developers</h1>
+				<h1 style="visibility:hidden">Mobile Developers</h1>
 				<h3>Mobile Developers</h3>
 			</section>
 
@@ -121,17 +122,17 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 									<p class="intern-desc"><?= $mobile['Role'] ?></p>
 									<p><?= $mobile['Stacks'] ?></p>
 								</div>
-								<div class="intern-card-back">
-									<p><?= $mobile['Name'] ?></p>
-									<p><?= $mobile['Role'] ?></p>
-									<button><a href="<?= $mobile['Portfolio'] ?>">View Portfolio</a></button>
-									<div class="social-links">
-										<a href="<?= $mobile['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
-										<a href="<?= $mobile['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
-										<a href="<?= $mobile['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
-										<a href="<?= $mobile['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
-										<a href="<?= $mobile['Dribbble'] ?>"><img src="../app/img/dribbble icon.png" alt="social link" title="Dribbble"></a>
-									</div>
+							</div>
+							<div class="intern-card-back">
+								<p><?= $mobile['Name'] ?></p>
+								<p><?= $mobile['Role'] ?></p>
+								<button><a href="<?= $mobile['Portfolio'] ?>">View Portfolio</a></button>
+								<div class="social-links">
+									<a href="<?= $mobile['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
+									<a href="<?= $mobile['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
+									<a href="<?= $mobile['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
+									<a href="<?= $mobile['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
+									<a href="<?= $mobile['Dribbble'] ?>"><img src="../app/img/dribbble icon.png" alt="social link" title="Dribbble"></a>
 								</div>
 							</div>
 						</div>
@@ -140,7 +141,7 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 			</div>
 
 			<section class="hng-track">
-				<h1>Machine Learning</h1>
+				<h1 style="visibility:hidden">Machine Learning</h1>
 				<h3>Machine Learning</h3>
 			</section>
 
@@ -157,16 +158,16 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 									<p class="intern-desc"><?= $ml['Role'] ?></p>
 									<p><?= $ml['Stacks'] ?></p>
 								</div>
-								<div class="intern-card-back">
-									<p><?= $ml['Name'] ?></p>
-									<p><?= $ml['Role'] ?></p>
-									<button><a href="<?= $ml['Portfolio'] ?>">View Portfolio</a></button>
-									<div class="social-links">
-										<a href="<?= $ml['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
-										<a href="<?= $ml['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
-										<a href="<?= $ml['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
-										<a href="<?= $ml['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
-									</div>
+							</div>
+							<div class="intern-card-back">
+								<p><?= $ml['Name'] ?></p>
+								<p><?= $ml['Role'] ?></p>
+								<button><a href="<?= $ml['Portfolio'] ?>">View Portfolio</a></button>
+								<div class="social-links">
+									<a href="<?= $ml['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
+									<a href="<?= $ml['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
+									<a href="<?= $ml['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
+									<a href="<?= $ml['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
 								</div>
 							</div>
 						</div>
@@ -175,7 +176,7 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 			</div>
 
 			<section class="hng-track">
-				<h1>Digital Marketers</h1>
+				<h1 style="visibility:hidden">Digital Marketers</h1>
 				<h3>Digital Marketers</h3>
 			</section>
 
@@ -192,27 +193,26 @@ $webs->parse(__DIR__ . '/storage/web.csv');
 									<p class="intern-desc"><?= $marketer['Role'] ?></p>
 									<p><?= $marketer['Stacks'] ?></p>
 								</div>
-								<div class="intern-card-back">
-									<p><?= $marketer['Name'] ?></p>
-									<p><?= $marketer['Role'] ?></p>
-									<button><a href="<?= $marketer['Portfolio'] ?>">View Portfolio</a></button>
-									<div class="social-links">
-										<a href="<?= $marketer['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
-										<a href="<?= $marketer['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
-										<a href="<?= $marketer['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
-										<a href="<?= $marketer['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
-									</div>
+							</div>	
+							<div class="intern-card-back">
+								<p><?= $marketer['Name'] ?></p>
+								<p><?= $marketer['Role'] ?></p>
+								<button><a href="<?= $marketer['Portfolio'] ?>">View Portfolio</a></button>
+								<div class="social-links">
+									<a href="<?= $marketer['LinkedIn'] ?>"><img src="../app/img/linkedin icon.png" alt="social link" title="LinkedIn"></a>
+									<a href="<?= $marketer['Medium'] ?>"><img src="../app/img/medium icon.png" alt="social link" title="Medium"></a>
+									<a href="<?= $marketer['GitHub'] ?>"><img src="../app/img/github icon.png" alt="social link" title="Github"></a>
+									<a href="<?= $marketer['Twitter'] ?>"><img src="../app/img/twitter icon.png" alt="social link" title="Twitter"></a>
 								</div>
-							</div>
+							</div>		
 						</div>
 					</div>
 				<?php } ?>
 			</div>
-
+		</main>
 			<div class="ap-footer">
 				<?php include 'partials/footer.php'; ?>
 			</div>
-		</main>
 		<!-- <script src="https://unpkg.com/axios/dist/axios.min.js"> </script>
 		                                    <script src="../app/js/finalist5.js"> </script> -->
 		</body>
