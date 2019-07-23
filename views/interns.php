aab6f9bdf4c8a929be315f6dca1670d046be9d0e
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-07-23 12:21:29 +0100

    slider init

--- views/interns.php
+++ views/interns.php
@@ -11,6 +11,93 @@
             <h3>Our Past Interns</h3>
 
         </header>
+
+        <!-- Logo Slider  -->
+        <div class="container">
+        <div class="wrapper">
+        <ul class="carousel" data-target="carousel">
+          <li class="card" data-target="card">
+                <a href="https://www.hotels.ng">
+                    <img src="hotels.ng.png" alt="hotels.ng logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="andela.png" alt="andela logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="freeme.png" alt="freeme logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="kodehauz-logo.png" alt="kodehauz-logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="sprinble.png" alt="sprinble logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="Tizeti.png" alt="Tizeti logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://www.hotels.ng">
+                    <img src="hotels.ng.png" alt="hotels.ng logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="andela.png" alt="andela logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="freeme.png" alt="freeme logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="kodehauz-logo.png" alt="kodehauz-logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="sprinble.png" alt="sprinble logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+                <a href="https://hotels.ng">
+                    <img src="Tizeti.png" alt="Tizeti logo" target="_blank" >
+                </a>
+          </li>
+          <li class="card" data-target="card">
+              <a href="https://hotels.ng">
+                <img src="hotels.ng.png" alt="hotels.ng logo" target="_blank" >
+              </a>
+          </li>
+          <li class="card" data-target="card">
+              <a href="https://hotels.ng">
+                <img src="hotels.ng.png" alt="hotels.ng logo" target="_blank" >
+              </a>
+          </li>
+          <li class="card" data-target="card">
+              <a href="https://hotels.ng">
+                <img src="hotels.ng.png" alt="hotels.ng logo" target="_blank" >
+              </a>
+          </li>
+        </ul>
+        <div class="button-wrapper">
+          <button data-action="slideLeft">&lt;</button>
+          <button data-action="slideRight">&gt;</button>
+        </div>
+      </div>
+    </div>
         
         <!-- Filters -->
         <!-- <div class="row filters">
@@ -68,7 +155,47 @@
 
 	<?php include 'partials/footer.php'; ?>
 
+    <script>
+          // Select the carousel you'll need to manipulate and the buttons you'll add events to
+            const carousel = document.querySelector("[data-target='carousel']");
+            const card = carousel.querySelector("[data-target='card']");
+            const leftButton = document.querySelector("[data-action='slideLeft']");
+            const rightButton = document.querySelector("[data-action='slideRight']");
 
+            // Prepare to limit the direction in which the carousel can slide, 
+            // and to control how much the carousel advances by each time.
+            // In order to slide the carousel so that only three cards are perfectly visible each time,
+            // you need to know the carousel width, and the margin placed on a given card in the carousel
+            const carouselWidth = carousel.offsetWidth;
+            const cardStyle = card.currentStyle || window.getComputedStyle(card)
+            const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+
+            // Count the number of total cards you have
+            const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+
+            // Define an offset property to dynamically update by clicking the button controls
+            // as well as a maxX property so the carousel knows when to stop at the upper limit
+            let offset = 0;
+            const maxX = -((cardCount / 5) * carouselWidth + 
+                        (cardMarginRight * (cardCount / 5)) - 
+                        carouselWidth - cardMarginRight);
+
+
+            // Add the click events
+            leftButton.addEventListener("click", function() {
+            if (offset !== 0) {
+                offset += carouselWidth + cardMarginRight;
+                carousel.style.transform = `translateX(${offset}px)`;
+                }
+            })
+            
+            rightButton.addEventListener("click", function() {
+            if (offset !== maxX) {
+                offset -= carouselWidth + cardMarginRight;
+                carousel.style.transform = `translateX(${offset}px)`;
+            }
+            })
+      </script>
 </body>
 
 
