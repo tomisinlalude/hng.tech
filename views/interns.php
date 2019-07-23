eae2e4dbc89997cd146794e9352377c558492d79
Seyi Onifade <xyluz@ymail.com>
2019-07-23 09:26:54 +0100

    move past interns data to data.json

--- views/interns.php
+++ views/interns.php
@@ -1,91 +1,17 @@
 <html>
 <?php include 'partials/header.php'; ?>
 
-<body class=" pi">
+<body class="pi">
 	<?php include 'partials/navbar.php'; ?>
 	<div class="container intern-body">
 
         <!-- Header -->
-        <header class="head"></header>
-            <h1> HNG Past Interns</h1>
-            <h3>Our Interns and the impacts they have made</h3>
+        <header class="head">
+         
+            <h3>Our Past Interns</h3>
+
         </header>
         
-         <!--Logo Carousel Wrapper-->
-    <div id="logo-carousel" class="carousel slide carousel-multi-item carousel-multi-item-2" data-ride="carousel">
-
-            <!--Controls-->
-            <!-- <div class="controls-top">
-              <a class="black-text" href="#logo-carousel" data-slide="prev"> <img src="../app/img/interns/arrow-left.svg" alt="left-scroll"> </a>
-              <a class="black-text" href="#logo-carousel" data-slide="next"><img src="../app/img/interns/arrow-right.svg" alt="left-scroll"></a>
-            </div> -->
-            
-            <!--/.Controls-->
-          
-            <!--Slides-->
-            <div class="carousel-inner" role="listbox">
-          
-              <!--First slide-->
-              <div class="carousel-item active">
-                    
-                <div class="col-md-3 mb-3">
-                  <div class="card">
-                    <img class="img-fluid" src="../app/img/interns/andela.png"
-                      alt="andela logo">
-                  </div>
-                </div>
-          
-          
-                <div class="col-md-3 mb-3">
-                  <div class="card">
-                    <img class="img-fluid" src="../app/img/interns/hotels.ng.png"
-                      alt="hotels.ng logo">
-                  </div>
-                </div>
-    
-                <div class="col-md-3 mb-3">
-                  <div class="card">
-                    <img class="img-fluid" src="../app/img/interns/gloopro.svg"
-                      alt="gloopro logo">
-                  </div>
-                </div>
-          
-              </div>
-              <!--/.First slide-->
-          
-              <!--Second slide-->
-              <div class="carousel-item">
-               
-          
-                <div class="col-md-3 mb-3">
-                  <div class="card">
-                    <img class="img-fluid" src="../app/img/interns/freeme.png"
-                      alt="freeme logo">
-                  </div>
-                </div>
-          
-                <div class="col-md-3 mb-3">
-                  <div class="card">
-                    <img class="img-fluid" src="../app/img/interns/kodehauz-logo.png"
-                      alt="kodehauz-logo">
-                  </div>
-                </div>
-    
-              
-          
-              </div>
-              <!--/.Second slide-->
-          
-              <!--Third slide-->
-              
-              <!--/.Third slide-->
-          
-            </div>
-            <!--/.Slides-->
-          
-          </div>
-          <!--/.Carousel Wrapper-->
-
         <!-- Filters -->
         <!-- <div class="row filters">
             <select class="select-css">
@@ -119,68 +45,23 @@
         </div> -->
         
         <div class="row cards interns">
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/gabumoh/image/upload/e_grayscale/v1557903161/gabumoh_rmmy8z.jpg" alt="Card image cap">
-                <p class="name">Gabriel Umoh</p>
-                <p class="stack">Backend / DevOps / PM</p>
-                <!-- <p>Internship 3</p> -->
-            </div>
-            <div class="intern">
-                <div class="img"><img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/djvzkeset/image/upload/e_grayscale/v1558225291/IMG_2550.jpg" alt="Card image cap"></div>
-                <p class="name">Oluwabukola Ajishebiyawo</p>
-                <p class="stack">Backend Developer<br />@Tizani</p>
-                
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/opheus/image/upload/e_grayscale/v1558065110/_BLE6686.jpg" alt="Card image cap">
-                <p class="name">Ominiabohs E David</p>
-                <p class="stack">Back End Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/samuelweke/image/upload/e_grayscale/v1523620154/2017-11-13_21.01.13.jpg" alt="Card image cap">
-                <p class="name">Weke Samuel Olusegun</p>
-                <p class="stack">Front End Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/drlcfqzym/image/upload/e_grayscale/v1557833609/LRM_EXPORT_69521434596572_20190428_192942547.jpg" alt="Card image cap">
-                <p class="name">Oghenero Paul-Ejukorlem</p>
-                <p class="stack">Full Stack Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/nedy123/image/upload/e_grayscale/v1558000126/my_dp_zz5dbu.jpg" alt="Card image cap">
-                <p class="name">Edidiong Udombat</p>
-                <p class="stack">Full Stack Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/dkkn9xzk7/image/upload/e_grayscale/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1557940249/Victor_tydbgo.jpg" alt="Card image cap">
-                <p class="name">Victor Ugwueze</p>
-                <p class="stack">Full Stack Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/dvggqzsvj/image/upload/e_grayscale/v1549669716/potrait_flkkue.jpg" alt="Card image cap">
-                <p class="name">Ikechukwu Nwakanma</p>
-                <p class="stack">Back end Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/samfield/image/upload/e_grayscale/v1554317342/samfield.jpg" alt="Card image cap">
-                <p class="name">Samfield Hawb</p>
-                <p class="stack">Full Stack Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/maaj/image/upload/e_grayscale/v1557996071/muheez-jimoh.jpg" alt="Card image cap">
-                <p class="name">Jimoh Muheez</p>
-                <p class="stack">Full Stack Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/dqscsuyyn/image/upload/e_grayscale/v1557837172/IMG_20181206_113029.jpg" alt="Card image cap">
-                <p class="name">Chigozie Ekwonu</p>
-                <p class="stack">Full Stack Developer</p>
-            </div>
-            <div class="intern">
-                <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/iamdharmy/image/upload/e_grayscale/v1557852684/Oluwadamilola%20Soyombo.jpg" alt="Card image cap">
-                <p class="name">Oluwadamilola Soyombo</p>
-                <p class="stack">Designer</p>
-            </div>
+
+<?php
+ 
+  $data = file_get_contents(__DIR__ . '/storage/data.json'); 
+  $interns = json_decode($data); 
+
+  foreach($interns as $intern){
+
+    echo "<div class='intern'><img class='card-img-top' style='object-fit: cover' src='".$intern->picture."' alt='Card image cap'><p class='name'>".$intern->name."</p>
+    <p class='stack'>".$intern->stack."<br />".$intern->role." @<a style='font-size: 12px' href='".$intern->company_url."'>".$intern->company."</a></p></div>";
+
+  }
+?>
+
+           
+<!--            
+           
             <div class="intern">
                 <img class="card-img-top" style=" object-fit: cover" src="https://res.cloudinary.com/gorge/image/upload/e_grayscale/v1557841999/FullSizeRender-8.jpg" alt="Card image cap">
                 <p class="name">Iruene  Adokiye</p>
@@ -347,7 +228,7 @@
                 <img class="card-img-top" style=" object-fit: cover" src="http://mho6nxweoblkn351jmpbb12y.wpengine.netdna-cdn.com/wp-content/uploads/2018/09/male-placeholder.jpg" alt="Card image cap">
                 <p class="name">Stephen Afam Osemene</p>
                 <p class="stack"> Software Engineer <br />@Africastalking</p>
-            </div>
+            </div> -->
 
         </div>
 
