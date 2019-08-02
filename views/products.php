5b6de0c911657e4e51eb3329c9cc15762bcceb00
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-02 14:00:15 +0100

    apoe4 removed

--- views/products.php
+++ views/products.php
@@ -6,29 +6,6 @@
 	<div class="container gridContainer">
 		<div class="row center-block">
 			<div class="jumbotron product col-sm-6 col-lg-4 col-lg-offset-2 col-md-4 "  id="productAnime">
-				<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="imgCenter center-block" style="width:50%;">
-				<div id="extra">
-					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1557018690/apoewhite.png" alt="apoe4_logo" class=" centre-block" style="width:50%; margin:50px 0px 0px 75px;">
-					<p class="text-center detail" >An innovative app that helps people with history of Apoe4 gene that are culpable to Alhzemier. This product helps users monitor their daily activities and provide tips from our medical researchers.</p>
-					<div class="text-center">
-						<a href="https://apoe4.app" target="blank"><button type="button" class="btn btn-custom mr-0">Learn more</button></a>
-					</div>
-				</div>
-			</div>
-			<div class="jumbotron product col-sm-6  col-lg-4  col-lg-offset-1 col-md-4 text-center" id="productAnime">
-				<img src="app/img/lucid.png" alt="Lucid-logo-color" class="imgCenter center-block" style="width:80%;">
-				<div id="extra">
-					<img src="app/img/lucidwhite.png" alt="Lucid-logo-white" class=" centre-block" style="width:80%; margin: 50px 0px 0px 0px;">
-					<p class="text-center detail">
-					The Ultimate LightWeight Content Platform. LUCID was built on the premise that ownership and privacy is about respect and that it is your right.</p>
-					<div class="text-center">
-						<a href="https://lucid.blog/" target="blank"><button type="button" class="btn btn-custom mr-0">Learn more</button></a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row center-block">
-			<div class="jumbotron product col-sm-6 col-lg-4 col-lg-offset-2 col-md-4 "  id="productAnime">
 				<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert-logo" class="imgCenter center-block" style="width:30%;">
 				<div id="extra">
 					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556547608/devalertlogo.jpg" alt="devalert-logo" class=" centre-block" style="width:30%; margin:50px 0px 0px 105px;">
@@ -48,6 +25,29 @@
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="row center-block">
+			<div class="jumbotron product col-sm-6  col-lg-4  col-lg-offset-2 col-md-4 text-center" id="productAnime">
+				<img src="app/img/lucid.png" alt="Lucid-logo-color" class="imgCenter center-block" style="width:80%;">
+				<div id="extra">
+					<img src="app/img/lucidwhite.png" alt="Lucid-logo-white" class=" centre-block" style="width:50%; margin:50px 0px 0px 75px;">
+					<p class="text-center detail">
+					The Ultimate LightWeight Content Platform. LUCID was built on the premise that ownership and privacy is about respect and that it is your right.</p>
+					<div class="text-center">
+						<a href="https://lucid.blog/" target="blank"><button type="button" class="btn btn-custom mr-0">Learn more</button></a>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="jumbotron product col-sm-6 col-lg-4 col-lg-offset-1 col-md-4 "  id="productAnime">
+				<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1556545120/apoe_logo.png" alt="apoe4_logo" class="imgCenter center-block" style="width:50%;">
+				<div id="extra">
+					<img src="https://res.cloudinary.com/dx6ol2ale/image/upload/v1557018690/apoewhite.png" alt="apoe4_logo" class=" centre-block" style="width:50%; margin:50px 0px 0px 75px;">
+					<p class="text-center detail" >An innovative app that helps people with history of Apoe4 gene that are culpable to Alhzemier. This product helps users monitor their daily activities and provide tips from our medical researchers.</p>
+					<div class="text-center">
+						<a href="https://apoe4.app" target="blank"><button type="button" class="btn btn-custom mr-0">Learn more</button></a>
+					</div>
+				</div>
+			</div> -->
 		</div>
 	</div>
 </body>
